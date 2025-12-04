4b941440e2221b4ceb7c9569210d83d2ac6f4955
Brian Steele <gobriansteele@gmail.com>
2025-12-04 15:54:54 -0600

    update next

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import nextConfig from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
+
+const eslintConfig = [...nextConfig, ...nextCoreWebVitals, ...nextTypeScript];
+
+export default eslintConfig;
